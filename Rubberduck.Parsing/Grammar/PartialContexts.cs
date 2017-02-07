34ef01e837865e5e25b1cfa946f0d81023bea321
Mathieu Guindon <retailcoder@gmail.com>
2017-02-07 10:30:47 -0500

    added more partial IInspectable contexts

--- Rubberduck.Parsing/Grammar/PartialContexts.cs
+++ Rubberduck.Parsing/Grammar/PartialContexts.cs
@@ -36,5 +36,77 @@ namespace Rubberduck.Parsing.Grammar
             public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
             public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
         }
+
+        public partial class FileNumberContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class CallStmtContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class ErrorStmtContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class ExpressionContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class DefTypeContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class GoSubStmtContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class GoToStmtContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class WhileWendStmtContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
+
+        public partial class RemCommentContext : IInspectable
+        {
+            private readonly InspectableContext _inspectable = new InspectableContext();
+            public IEnumerable<IInspectionResult> InspectionResults { get { return _inspectable.InspectionResults; } }
+            public void Annotate(IInspectionResult result) { _inspectable.Annotate(result); }
+            public void ClearInspectionResults() { _inspectable.ClearInspectionResults(); }
+        }
     }
 }
